bccdb0c0e5e4e9c26fd42a2e0579b4979c45ea7d
Pedro Teixeira <i@pgte.me>
2014-01-30 12:15:18 +0000

    v0.3.0

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function propagate(events, source, dest) {
   // events should be an array
   if (events && ! Array.isArray(events)) events = [events];
 
+  if (Array.isArray(events)) {
+    return explicitPropagate(events, source, dest);
+  }
+
   var oldEmit =  source.emit;
 
   source.emit = function(eventType) {
@@ -27,3 +31,32 @@ function propagate(events, source, dest) {
 };
 
 module.exports = propagate;
+
+function explicitPropagate(events, source, dest) {
+
+  var listeners = events.map(function(event) {
+    return function() {
+      var args = Array.prototype.slice(arguments);
+      args.unshift(event);
+      dest.emit.apply(dest, args);
+    }
+  });
+
+  listeners.forEach(register);
+
+  return {
+    end: end
+  };
+
+  function register(listener, i) {
+    source.on(events[i], listener);
+  }
+
+  function unregister(listener, i) {
+    source.removeListener(events[i], listener);
+  }
+
+  function end() {
+    listeners.forEach(unregister);
+  }
+}
